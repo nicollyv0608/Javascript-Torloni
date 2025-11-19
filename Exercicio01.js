0451fe8a152bf4b7b809bcd51a03e909409bedab
NICOLLY VIANA DOS SANTOS <54695662850@imaccommand003.local>
2025-11-19 11:40:32 -0300

    Exercicio 01

---
+++ Exercicio01.js
@@ -0,0 +1,58 @@
+// let usuario = prompt("nome de usuário":)
+
+// switch (usuario) {
+//     case "admin":
+//         console.log("Usuário correto!")
+//           break;
+
+
+//     default:
+//         console.log("Usuário incorreto.")
+//         break;
+        
+// }
+// let senha = prompt("insira a senha")
+
+// switch (senha) {
+//     case "senha 123":
+//         console.log("senha correta!!")
+
+//         break;
+
+//     default:
+//         console.log("senha incorreta")
+//         break;
+// }
+// window.alert("login feito com sucesso")
+// console.log("apresenta mensagem na tela")
+
+let usuario = prompt("nome de usuário: ")
+
+switch (usuario) {
+    case "admin":
+        console.log("usuário correto!")
+
+        break;
+
+
+
+    default:
+        console.log("usuário incorreto.")
+
+        break;
+}
+
+let senha = prompt("insira a senha")
+
+switch (senha) {
+    case "senha123":
+        console.log("senha correta!")
+        break;
+
+    default:
+        console.log("senha incorreta.")
+        break;
+}
+
+window.alert("Login feito com sucesso!")
+console.log("Apresenta mensagem na tela") .
