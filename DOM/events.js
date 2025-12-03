8bc8ced1a145b3e28dafe9d26724eefb8a05d52a
NICOLLY VIANA DOS SANTOS <54695662850@imaccommand003.local>
2025-12-03 14:32:39 -0300

    iniciado

--- DOM/events.js
+++ DOM/events.js
@@ -2,14 +2,23 @@
 //comportamento do website ,por exemplo carregar a tela
 
 let elementobtn = document.getElementById("btn");
+let elementoidleandra = document.getElementById("tecnica")
 let novoElemento = document.createElement("a")
 novoElemento.textContent = "nossa praia e amar 2| jorge e & matheus"
 novoElemento.href = "https://www.youtube.com/watch?v=_v0-5QY6y-M&list=RD_v0-5QY6y-M&start_radio=1"
 
 elementobtn.addEventListener("click", () =>{
 
+    // elementobtn.style.backgroundColor = "purple";
 
-    elementobtn.style.backgroundColor = "purple";
+    // document.body.appendChild(novoElemento)
 
-    document.body.appendChild(novoElemento)
+    document.body.appendChild(novoElemento);
+    // document.body.removeChild(elementoidleandra);
+    // console.log(document.body.children);
+
+    document.body.children[2].removeChild(elementoidleandra)
+
+    
+
 })
